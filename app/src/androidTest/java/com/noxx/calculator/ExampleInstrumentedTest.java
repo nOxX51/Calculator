1a9136c17d8f6ab8468e8abcb89cf3fb3dfe1c74
nOxX51 <noxx_51@hotmail.com>
2016-11-21 14:03:15 +0100

    ADD test UI

--- app/src/androidTest/java/com/noxx/calculator/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/noxx/calculator/ExampleInstrumentedTest.java
@@ -2,11 +2,19 @@ package com.noxx.calculator;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -15,12 +23,30 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.noxx.calculator", appContext.getPackageName());
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testAddition() {
+        onView(withId(R.id.button5)).perform(click());
+        onView(withId(R.id.buttonPlus)).perform(click());
+        onView(withId(R.id.button4)).perform(click());
+        onView(withId(R.id.buttonEgal)).perform(click());
+
+        onView(withId(R.id.text_field)).check(matches(withText("9.0")));
+    }
+
+    @Test
+    public void testMoins() {
+        onView(withId(R.id.button1)).perform(click());
+        onView(withId(R.id.button5)).perform(click());
+        onView(withId(R.id.buttonMoins)).perform(click());
+        onView(withId(R.id.button5)).perform(click());
+        onView(withId(R.id.buttonPlus)).perform(click());
+
+        onView(withId(R.id.text_field)).check(matches(withText("10.0")));
     }
 }
